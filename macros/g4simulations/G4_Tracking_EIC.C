65b3dfbd6508971d2bc707e268deb086cb044343
carlosperezlara <cperez@rcas2066.rcf.bnl.gov>
2018-09-13 13:25:38 -0400

    adding new EIC fast tracker

--- macros/g4simulations/G4_Tracking_EIC.C
+++ macros/g4simulations/G4_Tracking_EIC.C
@@ -2,6 +2,7 @@
 
 #include "G4_GEM_EIC.C"
 #include "G4_Svtx_maps_ladders+intt_ladders+tpc_KalmanPatRec_EIC.C"
+//#include "G4_Svtx_maps_ladders+intt_ladders+tpc_KalmanPatRec_EIC2.C"
 
 void TrackingInit(int verbosity = 0)
 {
@@ -19,19 +20,19 @@ double Tracking(PHG4Reco* g4Reco, double radius,
             const int absorberactive = 0,
             int verbosity = 0)
 {
+  /* Place central tracking detectors */
+  Svtx(g4Reco, radius);
+
   /* Place electron-going side tracking detectors */
   EGEMSetup(g4Reco);
 
   /* Place hadron-going side tracking detectors */
   FGEMSetup(g4Reco);
 
-  /* Place central tracking detectors */
-  Svtx(g4Reco, radius);
-
   return;
 }
 
-void Tracking_Reco(int verbosity = 0)
+void Tracking_Reco(int verbosity = 0, TString fileout="auto.root")
 {
   //---------------
   // Load libraries
@@ -50,49 +51,47 @@ void Tracking_Reco(int verbosity = 0)
   // Kalman Filter
   //---------------------
 
-  PHG4TrackFastSim* kalman = new PHG4TrackFastSim("PHG4TrackFastSim");
-  kalman->Verbosity(10);
+  PHG4TrackFastSim* kalman = new PHG4TrackFastSim("PHG4TrackFastSim",fileout.Data());
+  kalman->Verbosity(0);
 
-  kalman->set_use_vertex_in_fitting(true);
-  kalman->set_vertex_xy_resolution(50E-4);
-  kalman->set_vertex_z_resolution(50E-4);
-
-  //kalman->set_detector_type(PHG4TrackFastSim::Vertical_Plane); // Vertical_Plane, Cylinder
-  //kalman->set_phi_resolution(50E-4);
-  //kalman->set_r_resolution(1.);
-  //kalman->set_pat_rec_hit_finding_eff(1.);
-  //kalman->set_pat_rec_noise_prob(0.);
-  std::string phg4hits_names[] = {"G4HIT_SVTX","G4HIT_MAPS","G4HIT_EGEM_0","G4HIT_EGEM_1","G4HIT_EGEM_2","G4HIT_EGEM_3","G4HIT_FGEM_0","G4HIT_FGEM_1","G4HIT_FGEM_2","G4HIT_FGEM_3","G4HIT_FGEM_4"};
-  int dettypes[] = {PHG4TrackFastSim::Cylinder,PHG4TrackFastSim::Cylinder,
-		    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane,
-		    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane,
-		    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane,
-		    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane,
-		    PHG4TrackFastSim::Vertical_Plane };
-  float rad[] = {5.0,  100,  100,  100,  100,  100,  100,  100,  100,  100,  100};
-  float phi[] = {5.0,  150,  100,  100,  100,  100,  100,  100,  100,  100,  100};
-  float lon[] = {5.0,  200,  100,  100,  100,  100,  100,  100,  100,  100,  100};
-  for(int i=0; i!=11; ++i) { // from um to cm
+  kalman->VertexIn();
+  kalman->VertexSmear(50E-4,50E-4); // 50 micros
+  std::string phg4hits_names[16] = {"G4HIT_MAPS",   "G4HIT_SVTX",
+				    "G4HIT_EGEM_0", "G4HIT_EGEM_1",
+				    "G4HIT_EGEM_2O","G4HIT_EGEM_2I",
+				    "G4HIT_EGEM_3O","G4HIT_EGEM_3I",
+				    "G4HIT_FGEM_0", "G4HIT_FGEM_1",
+				    "G4HIT_FGEM_2O","G4HIT_FGEM_2I",
+				    "G4HIT_FGEM_3O","G4HIT_FGEM_3I",
+				    "G4HIT_FGEM_4O","G4HIT_FGEM_4I"};
+  int dettypes[16] = {
+    PHG4TrackFastSim::Cylinder, PHG4TrackFastSim::Cylinder, // SVTX TPC | phi, lon
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // EGEM01 | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // EGEM2  | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // EGEM3  | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // FGEM01 | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // FGEM2  | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane, // FGEM3  | rad, phi
+    PHG4TrackFastSim::Vertical_Plane,PHG4TrackFastSim::Vertical_Plane};// FGEM4  | rad, phi
+  float rad[16] = {5.0,  10000,10000,10000,10000,10000,10000,10000,
+		   10000,10000,10000,10000,10000,10000,10000,10000};
+  float phi[16] = {5.0,  150,  50,   50,  100,   50,  100,    50,
+		   50,   50,  100,   50,  100,   50,  100,    50};
+  float lon[16] = {5.0,  500,  5,    5,    5,    5,    5,    5,
+		   5,    5,    5,    5,    5,    5,    5,    5,};
+  for(int i=0; i!=16; ++i) { // from um to cm
     rad[i] *= 1e-4;
     phi[i] *= 1e-4;
     lon[i] *= 1e-4;
   }
-  float eff[] = {1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0};
-  float noi[] = {0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0};
-  kalman->set_phg4hits_names(phg4hits_names, dettypes, rad, phi, lon, eff, noi, 11);
-
-  //std::string phg4hits_names[] = {"G4HIT_EGEM_0","G4HIT_EGEM_1","G4HIT_EGEM_2","G4HIT_EGEM_3","G4HIT_FGEM_0","G4HIT_FGEM_1","G4HIT_FGEM_2","G4HIT_FGEM_3","G4HIT_FGEM_4"};
-  //kalman->set_phg4hits_names(phg4hits_names, 9);
-  kalman->set_sub_top_node_name("SVTX");
-  kalman->set_trackmap_out_name("SvtxTrackMap");
-
-  //std::string state_names[] = {"FEMC","FHCAL"};
-  //kalman->set_state_names(state_names, 2);
-
-  kalman->set_fit_alg_name("KalmanFitterRefTrack");//
-  kalman->set_primary_assumption_pid(13);
-  kalman->set_do_evt_display(false);
-
+  
+  float eff[16] = {1.0,1.0, 
+		   1.0,1.0,1.0,1.0,1.0,1.0,
+		   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
+  float noi[16] = {0.0,0.0,
+		   0.0,0.0,0.0,0.0,0.0,0.0,
+		   0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+  kalman->Configure(phg4hits_names, dettypes, rad, phi, lon, eff, noi, 16);
   se->registerSubsystem(kalman);
 
   return;
@@ -100,6 +99,7 @@ void Tracking_Reco(int verbosity = 0)
 
 void Tracking_Eval(std::string outputfile, int verbosity = 0)
 {
+  return;
   //---------------
   // Load libraries
   //---------------
